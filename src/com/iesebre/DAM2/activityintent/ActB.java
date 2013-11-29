22a1367627e561d479cdd9d4d6b38a170517a068
Josep Borrell Sánchez <josepborrell@iesebre.com>
2013-11-29 22:22:49 +0100

    ja esta tot

--- src/com/iesebre/DAM2/activityintent/ActB.java
+++ src/com/iesebre/DAM2/activityintent/ActB.java
@@ -1,18 +1,27 @@
 package com.iesebre.DAM2.activityintent;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ActB extends Activity {
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activitatb);
-		
+		  
+		    TextView edit_message = (TextView) findViewById(R.id.textView1);
+		    
+		    Bundle bundle = this.getIntent().getExtras();  
+		    		
+		    edit_message.setText(bundle.getString("text"));		    
+		    
+		    
 		Button orderButton = (Button)findViewById(R.id.button1);
-		
 		orderButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
--- src/com/iesebre/DAM2/activityintent/MainActivity.java
+++ src/com/iesebre/DAM2/activityintent/MainActivity.java
@@ -12,19 +12,22 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	 public final static String EXTRA_MESSAGE = "com.iesebre.DAM2.activityintent.MESSAGE";
-
+	 
 	
 	  public void onCreate(Bundle savedInstanceState) {
 		    super.onCreate(savedInstanceState);
 		    setContentView(R.layout.activity_main);
 
-		    Button orderButton = (Button)findViewById(R.id.button1);
-
+		    final Button orderButton = (Button)findViewById(R.id.button1);
+		    final EditText text = (EditText)findViewById(R.id.edit_message);
 		    orderButton.setOnClickListener(new View.OnClickListener() {
 
 		      @Override
 		      public void onClick(View view) {
 		        Intent intent = new Intent(MainActivity.this, ActB.class);
+		        Bundle b = new Bundle();
+		        b.putString("text",text.getText().toString());
+		        intent.putExtras(b);
 		        startActivity(intent);
 		      }
 
